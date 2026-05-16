ab81e1b5c977ff5c760e5b2c6a916c42362c9c33
Denmark B Rivera <denmarkbarbarona13@gmail.com>
2026-05-17 01:11:21 +0800

    Fix vite config base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/E-Portfolio/',
   plugins: [vue(), tailwindcss(), vueDevTools()],
   resolve: {
     alias: {
